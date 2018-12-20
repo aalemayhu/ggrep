738d837b1d74178bd0dcc7f44ef9c954570acdc4
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-20 22:29:55 +0100

    Default to searching in the current directory
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- packages/ggrep/ggrep.js
+++ packages/ggrep/ggrep.js
@@ -14,32 +14,34 @@ var repository_path = function(directory) {
     return directory;
 }
 
+var search = function(repository, keyword) {
+    const repo = repository_path(repository);
+    // TODO: rename keyword to term
+    var first = true;
+    var index = 0;
+    gitGrep(repo, {
+        rev: "HEAD",
+        term: keyword
+    }).on("data", function(data) {
+        if (first) {
+            console.log("%s\t%s\t\t%s\t\%s", chalk.yellow.underline("Index"), chalk.blue.underline("File"), chalk.green.underline("Line"), chalk.underline("Content"))
+            first = false;
+        }
+        // TODO: use zebra coloring on index
+        // TODO: should we highlight all occurences of the keyword?
+        console.log('%s\t%s\t\t%s\t\t%s', chalk.yellow(index++), chalk.blue(data.file), chalk.green(data.line), data.text.replace(keyword, chalk.bold.red(keyword)));
+    }).on("error", function(err) {
+        throw err;
+    }).on("end", function() {
+        // The end.
+    });
+}
+
 program.command('local')
     .option('-d, --directory <directory>', 'Use local git repository')
     .option('-k, --keyword <keyword>', 'Keyword to look for')
     .action((cmd) => {
-        var repository = repository_path(cmd["directory"]);
-        const keyword = cmd["keyword"];
-        var first = true;
-        var index = 0;
-
-        gitGrep(repository, {
-            rev: "HEAD",
-            term: keyword
-        }).on("data", function(data) {
-            if (first) {
-                console.log("%s\t%s\t\t%s\t\%s", chalk.yellow.underline("Index"), chalk.blue.underline("File"), chalk.green.underline("Line"), chalk.underline("Content"))
-                first = false;
-            }
-            // TODO: use zebra coloring on index
-            // TODO: should we highlight all occurences of the keyword?
-            console.log('%s\t%s\t\t%s\t\t%s', chalk.yellow(index++), chalk.blue(data.file), chalk.green(data.line), data.text.replace(keyword, chalk.bold.red(keyword)));
-        }).on("error", function(err) {
-            throw err;
-        }).on("end", function() {
-            // The end.
-        });
-
+        search(cmd["directory"], cmd["keyword"]);
     })
 
 program.command('remote <repo>')
@@ -48,5 +50,11 @@ program.command('remote <repo>')
         console.log('Remote=%s', remote);
         // TODO: Make sure valid protocol can be inferred.
     })
+
+// Default behviour should be to search in the current directory
+program.command('*').action((term) => {
+    search('.', term)
+});
+
 
 program.parse(process.argv);
