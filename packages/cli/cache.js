3d59ab11d93d4171f5e0d6a716512eae15af0ae6
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-21 17:26:07 +0100

    cli: put username in cache folder
    This makes it more unique but ideally we should use home directory.  The
    only reason I am putting it here now is so we don't need to deal with
    the cleanup.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- packages/cli/cache.js
+++ packages/cli/cache.js
@@ -1,23 +1,27 @@
 const path = require("path");
 const fs = require("fs");
+const os = require('os');
 
 class GGCache {
 	constructor() {
-	this.cacheDirectory = path.join("/tmp/", "ggrep-cache");
+	const user = os.userInfo().username;
+	this.cacheDirectory = path.join("/tmp/", `ggrep-cache-for-${user}`);
 	this.defaultConfigFile = path.join(this.cacheDirectory, "config.json");
 	this.DefaultConfig = { term: undefined, repository: undefined };
+
+	this.setup();
 	}
 
 	setup() {
-		if (fs.existsSync(cacheDirectory) === false) {
-			fs.mkdirSync(cacheDirectory);
+		if (fs.existsSync(this.cacheDirectory) === false) {
+			fs.mkdirSync(this.cacheDirectory);
 		}  
 	
-		if (fs.existsSync(defaultConfigFile)) {
+		if (fs.existsSync(this.defaultConfigFile)) {
 			try {
-				const data = fs.readFileSync(defaultConfigFile, "utf-8");
+				const data = fs.readFileSync(this.defaultConfigFile, "utf-8");
 				const tmp = JSON.parse(data);
-				DefaultConfig = tmp;
+				this.DefaultConfig = tmp;
 			} catch (err) {
 				console.log("Error in Cache:", err);
 			}    
