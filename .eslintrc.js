c1467d0a509b02b8dc768c93aeb50adb50693d39
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-29 17:40:31 +0100

    eslint: ignore identation
    The eslint is complaining too much and I don't care enough.  It can
    always be turned on later.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,16 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    node: true,
+    commonjs: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
